edb81d48cdbfaffbe01d957d800d2cd46378eef4
Venkata Ravila <venkataravila@gmail.com>
2020-02-29 18:07:06 -0600

    Created Navigation.js for temporary Header

--- muse-news/src/App.js
+++ muse-news/src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/Navigation';
 
 function App() {
+
   return (
-    <div className="App">
-      <link
-  rel="stylesheet"
-  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-  crossorigin="anonymous"
-/>
+      <div className="App">
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+            crossorigin="anonymous"
+          />
+
+          <Navigation />
+
       <header className="App-header">
         <p>
           Muse News
