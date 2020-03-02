f45280f7757026b9debb57c544ccd7ff39231c25
Kedar Raman <kedar.v.raman@gmail.com>
2020-03-02 14:34:53 -0600

    Artist model and instances working except for images and artists without an MBID in LastFM database

--- muse-news/src/components/ArtistDetail.js
+++ muse-news/src/components/ArtistDetail.js
@@ -1,18 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-function ArtistsPage() {
+function ArtistsPage({ match }) {
   useEffect(() => {
-    fetchItems();
+    fetchItem();
+    // console.log(match);
   }, []);
 
-  const [item, setItem] = useState([]);
+  const [item, setItem] = useState({
+    bio: {},
+    image: {
+      size: {},
+      text: {}
+    }
+  });
 
-  const fetchItems = async () => {};
+  const fetchItem = async () => {
+    const fetchItem = await fetch(
+      `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&mbid=${match.params.id}&api_key=${process.env.REACT_APP_LASTFM_API_KEY}&format=json`
+    );
+    const item = await fetchItem.json();
+    setItem(item.artist);
+    console.log(item.artist);
+  };
 
   return (
     <div>
-      <h4>Item</h4>
+      <h4>{item.name}</h4>
+      <img src={item.image.small} alt="" />
+      <p>{item.bio.content}</p>
     </div>
     // <div>
     //   <h1>Artists Page!!!</h1>
