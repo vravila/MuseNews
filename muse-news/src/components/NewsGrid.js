cb0771e4a39a975712e7ac9a7272138ea95a6a97
sDauenbaugh <s.dauenbaugh@gmail.com>
2020-03-05 20:07:17 -0600

    news pages

--- muse-news/src/components/NewsGrid.js
+++ muse-news/src/components/NewsGrid.js
@@ -7,15 +7,15 @@ class NewsGrid extends Component{
 
     state = {
         articles: [
-            {id: 1, title: 'Title One', preview: "This is a preview one"},
-            {id: 2, title: 'Title Two', preview: "This is a preview two"},
-            {id: 3, title: 'Title Three', preview: "This is a preview three"}
+            {id: 1, title: 'Noah Cyrus Reveals the Billie Eilish Song That Resonates With Her Most: Takeover Tuesday Playlist', preview: "Eilish is one of 15 artists Cyrus shouts out on her playlist.", url: 'https://www.billboard.com/articles/columns/pop/9327253/noah-cyrus-playlist-takeover-tuesday'},
+            {id: 2, title: 'Tame Impala’s ‘The Slow Rush’ Is Forgettable At Best', preview: "This is one half of the arts department’s review of Tame Impala’s The Slow Rush. The second article can be found here.", url: 'https://cornellsun.com/2020/03/01/tame-impalas-the-slow-rush-is-forgettable-at-best/'},
+            {id: 3, title: 'The Weeknd Gets Dark in New After Hours Short Film — with a Cameo by Jimmy Kimmel!', preview: "The video comes with an epilepsy trigger warning", url: 'https://people.com/music/weeknd-after-hours-short-film-jimmy-kimmel-cameo/'}
         ]
     };
     
     render(){
         const stories = this.state.articles.map(article=> 
-            <NewsArticle key={article.id} title={article.title} preview={article.preview} />);
+            <NewsArticle key={article.id} id={article.id} title={article.title} preview={article.preview} />);
         const disp = React.Children.toArray(stories);
         return(
             <div style={{marginLeft:20}}>
