0b4ff50edc324429c173f100349de4c96da52873
sDauenbaugh <s.dauenbaugh@gmail.com>
2020-03-06 13:29:21 -0600

    news working

--- muse-news/src/components/Article.js
+++ muse-news/src/components/Article.js
@@ -1,22 +1,40 @@
 import React, {Component} from 'react';
-import { Link } from "react-router-dom";
 
 class Article extends Component {
-    state = {
-        key: this.props.key,
-        title: "Title",
-        text: "Sample Text",
-        url: "www.google.com"
+        state = {
+            key: this.props.key,
+            title: 'Title',
+            text: 'text',
+            url: 'https://www.google.com'
+        }
+
+    updateState(){
+        //if(this.state.key === '1'){
+            this.state = {
+                title: 'Here’s Billie Eilish’s ‘Bad Guy’ in the style of Arctic Monkeys’ Alex Turner',
+                text: 'He likes when you get mardy',
+                url: 'https://www.nme.com/news/music/billie-eilish-bad-guy-alex-turner-style-2619882?amp'
+            };
+        /*else if(this.state.key == 2){
+            this.state.title = "";
+            this.state.text = "";
+            this.state.url = "";
+        }else{
+            this.state.title = "";
+            this.state.text = "";
+            this.state.url = "";
+        }*/
     }
 
     render() { 
+        this.updateState();
         return (
             <div>
                 <body>
                     <div style={{margin:20}}>
                         <h1>{this.state.title}</h1>
                         <p>{this.state.text}</p>
-                        <Link to={this.state.url}>Source</Link>
+                        <a href={this.state.url}>Source</a>
                     </div>
                 </body>
             </div>
--- muse-news/src/components/pages/NewsPage.js
+++ muse-news/src/components/pages/NewsPage.js
@@ -3,23 +3,38 @@ import {useParams} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Article from './../Article';
 
+function updateState(key){
+    if(key === '1'){
+        var title = 'Here’s Billie Eilish’s ‘Bad Guy’ in the style of Arctic Monkeys’ Alex Turner';
+        var text = 'He likes when you get mardy';
+        var url = 'https://www.nme.com/news/music/billie-eilish-bad-guy-alex-turner-style-2619882?amp';
+    }else if(key === '2'){
+        title = "Tame Impala’s Kevin Parker says he tried to give ‘The Less I Know The Better’ to Mark Ronson";
+        text = "I don’t know if he knows how successful it is…";
+        url = "https://www.nme.com/news/music/tame-impala-kevin-parker-the-less-i-know-the-better-to-mark-ronson-2599062";
+    }else{
+        title = "Watch Behind-the-Scenes Video From The Weeknd’s ‘Blinding Lights’ (EXCLUSIVE)";
+        text = "What’s not in the Anton Tammi-directed video, or perhaps not obvious, are the following little-known facts";
+        url = "https://variety.com/2020/music/news/the-weeknd-blinding-lights-behind-the-scenes-video-exclusive-1203518570/";
+    }
+    return({title, text, url});
+}
+
 function NewsPage(){
 
     let {key} = useParams();
-
-    return(
+    var {title, text, url} = updateState(key);
+    return (
         <div>
-            <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                crossorigin="anonymous"
-            />
-
-            <Article key={key} />
-
-            </div>
-    )
+            <body>
+                <div style={{margin:20}}>
+                    <h1>{title}</h1>
+                    <p>{text}</p>
+                    <a href={url}>Source</a>
+                </div>
+            </body>
+        </div>
+    );
 }
 
 export default NewsPage;
