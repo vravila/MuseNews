828429350b4a2aa4b46e0d6bc002b18cdf77da88
Venkata Ravila <venkataravila@gmail.com>
2020-04-24 19:43:19 -0500

    Fixed links issue

--- muse-news/client/src/components/Info.js
+++ muse-news/client/src/components/Info.js
@@ -3,7 +3,9 @@ import { InfoConsumer } from "./context";
 import { Link } from "react-router-dom";
 
 class Info extends Component {
+
   render() {
+    
     const {
       id,
       headerTitle,
@@ -11,10 +13,13 @@ class Info extends Component {
       headerText,
       img
     } = this.props.item;
-
+    
+    
     return (
       <InfoConsumer>
+        
         {value => (
+          
           <div className="col-10 col-lg-3 mx-auto mb-5">
             <div className="card" style={{ width: "20rem", height: "30rem" }}>
               <img
@@ -23,25 +28,80 @@ class Info extends Component {
                 className="card-img-top center-block"
                 style={{ width: "19rem", height: "15rem" }}
               />
-
+              
+            
               <div className="card-body">
                 <h3 className="card-title text-uppecase">{headerTitle}</h3>
                 <h5 className="card-title">{headerSubTitle}</h5>
                 <p className="card-text">{headerText}</p>
                 
-                  <Link
-                  to={`/${headerTitle}/splash/none/rank/none/none/none/none/none/none/none/1`}
+                {(() => {
+                      switch (headerTitle) {
+                        case "About":   
+                            return <Link
+                            to={`About/1`}
+                            className="btn btn-outline-primary text-uppercase"
+                          >
+                            More Info
+                          </Link>
+                        case "Songs":   
+                          return <Link
+                            to={`/Songs/splash/none/rank/none/none/none/none/none/none/none/1`}
+                            className="btn btn-outline-primary text-uppercase"
+                          >
+                            More Info
+                          </Link>
+                        case "News":    
+                          return <Link
+                          to={`/News`}
+                          className="btn btn-outline-primary text-uppercase"
+                        >
+                          More Info
+                        </Link>
+                        case "Artists": 
+                        return <Link
+                        to={`/Artists/splash/none/rank/false/none/none/none/none/1`}
+                        className="btn btn-outline-primary text-uppercase"
+                       >
+                        More Info
+                      </Link>
+                        default:        
+                        return <Link
+                        to={`/test`}
+                        className="btn btn-outline-primary text-uppercase"
+                      >
+                        More Info
+                      </Link>
+                      }
+                    })()}
+                {/* <Link
+                  to={`/${path}`}
                   className="btn btn-outline-primary text-uppercase"
                 >
                   More Info
                 </Link>
-                
+                 */}
               </div>
             </div>
           </div>
         )}
       </InfoConsumer>
+      
     );
+
+    
+
+    function getPath({ headerTitle }) {
+      
+      switch (headerTitle) {
+        case "About":   return "About/1";
+        case "Songs":   return"Songs/splash/none/rank/none/none/none/none/none/none/none/1";
+        case "News":    return "News";
+        case "Artists": return "Artists/splash/none/rank/false/none/none/none/none/1";
+        default:        return "test";
+      }
+
+    }
   }
 }
 
