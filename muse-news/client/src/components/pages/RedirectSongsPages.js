1e25f86fdef794010ac7d8ae6f5bb18c63d9ea3b
Kedar Raman <kedar.v.raman@gmail.com>
2020-05-01 14:18:23 -0500

    Refactor: Extract method- extract code that creates a URL from render method to createURLFromProps

--- muse-news/client/src/components/pages/RedirectSongsPages.js
+++ muse-news/client/src/components/pages/RedirectSongsPages.js
@@ -16,105 +16,7 @@ class RedirectPages extends Component {
     console.log(values.search === "");
     console.log(values.source);
 
-    var urlString = "/" + values.source;
-    if (values.search === "") {
-      console.log("search null");
-      urlString += "/splash/none";
-    } else {
-      var encodedSearch = encodeURIComponent(encodeURIComponent(values.search));
-      console.log("encoded url is" + encodedSearch);
-      urlString += "/search/" + encodedSearch;
-    }
-
-    if (values.sort === "rank") {
-      urlString += "/rank";
-    } else if (values.sort === "nameAsc") {
-      urlString += "/nameAsc";
-    } else if (values.sort === "nameDesc") {
-      urlString += "/nameDesc";
-    } else {
-      console.log("Illegal sort");
-      urlString += "/rank";
-    }
-
-    if (values.artistSearch === "") {
-      console.log("artist search null");
-      urlString += "/none";
-    } else {
-      var encodedSearch = encodeURIComponent(
-        encodeURIComponent(values.artistSearch)
-      );
-      console.log("encoded url is" + encodedSearch);
-      urlString += "/" + encodedSearch;
-    }
-
-    console.log(values.minPlayCount);
-    console.log(typeof values.minPlayCount);
-
-    if (values.minPlayCount === "") {
-      console.log("no min play count");
-      urlString += "/none";
-    } else {
-      console.log("Min Play count " + values.minPlayCount);
-      urlString += "/" + values.minPlayCount;
-    }
-
-    if (values.maxPlayCount === "") {
-      console.log("no max play count");
-      urlString += "/none";
-    } else {
-      console.log("max Play count " + values.maxPlayCount);
-      urlString += "/" + values.maxPlayCount;
-    }
-
-    if (values.minListeners === "") {
-      console.log("no min Listeners");
-      urlString += "/none";
-    } else {
-      console.log("Min Listeners " + values.minListeners);
-      urlString += "/" + values.minListeners;
-    }
-
-    if (values.maxListeners === "") {
-      console.log("no max Listeners");
-      urlString += "/none";
-    } else {
-      console.log("max Listeners " + values.maxListeners);
-      urlString += "/" + values.maxListeners;
-    }
-
-    if (values.minRank === "") {
-      console.log("no min Listeners");
-      urlString += "/none";
-    } else {
-      console.log("Min Listeners " + values.minRank);
-      urlString += "/" + values.minRank;
-    }
-
-    if (values.maxRank === "") {
-      console.log("no max Listeners");
-      urlString += "/none";
-    } else {
-      console.log("max Listeners " + values.maxRank);
-      urlString += "/" + values.maxRank;
-    }
-
-    console.log(values.page);
-    console.log(values.page === undefined);
-
-    if (
-      values.page === "" ||
-      values.page === undefined ||
-      values.page === null
-    ) {
-      console.log("no valid p age");
-      urlString += "/1";
-    } else {
-      console.log("Page " + values.page);
-      urlString += "/" + values.page;
-    }
-
-    console.log(urlString);
+    var urlString = createURLFromProps(values);
 
     return (
       <div>
@@ -122,6 +24,106 @@ class RedirectPages extends Component {
       </div>
     );
   }
+}
+
+function createURLFromProps(values) {
+  var urlString = "/" + values.source;
+  if (values.search === "") {
+    console.log("search null");
+    urlString += "/splash/none";
+  } else {
+    var encodedSearch = encodeURIComponent(encodeURIComponent(values.search));
+    console.log("encoded url is" + encodedSearch);
+    urlString += "/search/" + encodedSearch;
+  }
+
+  if (values.sort === "rank") {
+    urlString += "/rank";
+  } else if (values.sort === "nameAsc") {
+    urlString += "/nameAsc";
+  } else if (values.sort === "nameDesc") {
+    urlString += "/nameDesc";
+  } else {
+    console.log("Illegal sort");
+    urlString += "/rank";
+  }
+
+  if (values.artistSearch === "") {
+    console.log("artist search null");
+    urlString += "/none";
+  } else {
+    var encodedSearch = encodeURIComponent(
+      encodeURIComponent(values.artistSearch)
+    );
+    console.log("encoded url is" + encodedSearch);
+    urlString += "/" + encodedSearch;
+  }
+
+  console.log(values.minPlayCount);
+  console.log(typeof values.minPlayCount);
+
+  if (values.minPlayCount === "") {
+    console.log("no min play count");
+    urlString += "/none";
+  } else {
+    console.log("Min Play count " + values.minPlayCount);
+    urlString += "/" + values.minPlayCount;
+  }
+
+  if (values.maxPlayCount === "") {
+    console.log("no max play count");
+    urlString += "/none";
+  } else {
+    console.log("max Play count " + values.maxPlayCount);
+    urlString += "/" + values.maxPlayCount;
+  }
+
+  if (values.minListeners === "") {
+    console.log("no min Listeners");
+    urlString += "/none";
+  } else {
+    console.log("Min Listeners " + values.minListeners);
+    urlString += "/" + values.minListeners;
+  }
+
+  if (values.maxListeners === "") {
+    console.log("no max Listeners");
+    urlString += "/none";
+  } else {
+    console.log("max Listeners " + values.maxListeners);
+    urlString += "/" + values.maxListeners;
+  }
+
+  if (values.minRank === "") {
+    console.log("no min Listeners");
+    urlString += "/none";
+  } else {
+    console.log("Min Listeners " + values.minRank);
+    urlString += "/" + values.minRank;
+  }
+
+  if (values.maxRank === "") {
+    console.log("no max Listeners");
+    urlString += "/none";
+  } else {
+    console.log("max Listeners " + values.maxRank);
+    urlString += "/" + values.maxRank;
+  }
+
+  console.log(values.page);
+  console.log(values.page === undefined);
+
+  if (values.page === "" || values.page === undefined || values.page === null) {
+    console.log("no valid p age");
+    urlString += "/1";
+  } else {
+    console.log("Page " + values.page);
+    urlString += "/" + values.page;
+  }
+
+  console.log(urlString);
+
+  return urlString;
 }
 
 export default RedirectPages;
